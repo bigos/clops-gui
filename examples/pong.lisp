0315ff37fa6a2eb0f5a0fcb55c85b95b9aac6280
Jacek Podkanski <ruby.object@googlemail.com>
2024-05-11 17:10:04 +0100

    added basic events

--- examples/pong.lisp
+++ examples/pong.lisp
@@ -85,6 +85,46 @@
     (:timeout
      ;; do nothing yet
      )
+    (:menu-simple
+     (destructuring-bind ((menu-item)) args
+       (warn "menu item ~s" menu-item)
+       (cond
+         ((equal menu-item "quit")
+          (gui-window:close-all-windows-and-quit))
+
+         ((equal menu-item "about")
+          (gui-window:present-about-dialog
+           (list
+            :authors (list "Jacek Podkanski")
+            :website "https://github.com/bigos"
+            :program-name "Pong"
+            :comments "Nothing to say yet"
+            :license "Public Domain"
+            :system-information (format nil "~A" (uiop/os:implementation-identifier))
+            :logo-icon-name "application-x-addon")))
+
+         (T
+          (warn "not processed event ~S ~S" event args)))))
+    (:menu-bool (warn "not processed event ~S ~S" event args))
+    (:menu-radio (warn "not processed event ~S ~S" event args))
+    ((:motion :motion-enter) ; we use simple case with one window so we ignore the window argument
+     (destructuring-bind ((x y)) args
+       (setf (gui-window:mouse-coordinates gui-window:*lisp-app*) (cons x y)
+             (gui-window:current-motion    gui-window:*lisp-app*) lisp-window)))
+    (:motion-leave
+     (setf (gui-window:mouse-coordinates gui-window:*lisp-app*) nil
+           (gui-window:current-motion gui-window:*lisp-app*) nil))
+    (:focus-enter)
+    (:focus-leave)
+    (:pressed)
+    (:released)
+    (:scroll)
+    (:resize
+     (destructuring-bind ((w h)) args
+       (gui-window:window-resize w h lisp-window)))
+    (:key-pressed
+     (destructuring-bind ((entered key-name key-code mods)) args
+       (format t "~&>>> key pressed ~S~%" (list entered key-name key-code mods))))
     (otherwise
      (warn "not handled event ~S ~S" event args)))
 
@@ -95,12 +135,12 @@
 
 ;;; === main ===================================================================
 (defun main ()
-  (setf gui-window:*client-fn-menu-bar*      'pong::menu-bar
-        gui-window:*client-fn-draw-objects*  'pong::draw-window
-        gui-events:*client-fn-process-event* 'pong::process-event
-        gui-window:*initial-window-width*    600
-        gui-window:*initial-window-height*   400
-        gui-window:*initial-title*           "Pong")
+    (setf gui-window:*client-fn-menu-bar*      'pong::menu-bar
+          gui-window:*client-fn-draw-objects*  'pong::draw-window
+          gui-events:*client-fn-process-event* 'pong::process-event
+          gui-window:*initial-window-width*    600
+          gui-window:*initial-window-height*   400
+          gui-window:*initial-title*           "Pong")
 
   (gui-window:window (make-instance 'gui-window:lisp-window)))
 
