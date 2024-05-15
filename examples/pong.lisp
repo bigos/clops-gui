b322e1c7c4b6ffa06b1d7c621f52cbffed26342a
Jacek Podkanski <ruby.object@googlemail.com>
2024-05-15 23:31:28 +0100

    cleanup

--- examples/pong.lisp
+++ examples/pong.lisp
@@ -117,39 +117,39 @@
 ;;; === menu declaration =======================================================
 
 (defun menu-bar (app lisp-window)
-    (let ((menu (gio:make-menu)))
-      (gui-menu:build-menu
-       menu
+  (let ((menu (gio:make-menu)))
+    (gui-menu:build-menu
+     menu
 
-       (gui-menu:prepare-submenu
-        "File"
-        (gui-menu:prepare-section
-         nil
-         (gui-menu:build-items
-          (gui-menu:prepare-item-simple lisp-window app menu "Restart" "restart")))
-        (gui-menu:prepare-section
-         nil
-         (gui-menu:build-items
-          (gui-menu:prepare-item-simple lisp-window app menu "Quit" "quit")))
-        ;; end of prepare-submenu File
-        )
+     (gui-menu:prepare-submenu
+      "File"
+      (gui-menu:prepare-section
+       nil
+       (gui-menu:build-items
+        (gui-menu:prepare-item-simple lisp-window app menu "Restart" "restart")))
+      (gui-menu:prepare-section
+       nil
+       (gui-menu:build-items
+        (gui-menu:prepare-item-simple lisp-window app menu "Quit" "quit")))
+      ;; end of prepare-submenu File
+      )
 
-       (gui-menu:prepare-submenu
-        "Help"
-        (gui-menu:prepare-section
-         nil
-         (gui-menu:build-items
-          (gui-menu:prepare-item-simple lisp-window app menu "Tutorial" "tutorial")))
-        (gui-menu:prepare-section
-         nil
-         (gui-menu:build-items
-          (gui-menu:prepare-item-simple lisp-window app menu "About" "about")))
-        ;; end of prepare-submenu Help
-        )
-       ;; end of build-menu
-       )
+     (gui-menu:prepare-submenu
+      "Help"
+      (gui-menu:prepare-section
+       nil
+       (gui-menu:build-items
+        (gui-menu:prepare-item-simple lisp-window app menu "Tutorial" "tutorial")))
+      (gui-menu:prepare-section
+       nil
+       (gui-menu:build-items
+        (gui-menu:prepare-item-simple lisp-window app menu "About" "about")))
+      ;; end of prepare-submenu Help
+      )
+     ;; end of build-menu
+     )
 
-      (values menu)))
+    (values menu)))
 
 ;;; === drawing ================================================================
 
