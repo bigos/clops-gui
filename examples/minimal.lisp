41270dc043019325c0195fa8039e5eb190e48a2b
Jacek Podkanski <ruby.object@googlemail.com>
2024-10-04 01:37:35 +0100

    not working minimal file

---
+++ examples/minimal.lisp
@@ -0,0 +1,96 @@
+(declaim (optimize (speed 0) (safety 2) (debug 3)))
+
+;;;; Example of minimal window
+
+;;; load ===================================================================
+;; (load "~/Programming/Lisp/clops-gui/examples/minimal.lisp")
+(push #p "~/Programming/Lisp/clops-gui/" ql:*local-project-directories*)
+(ql:quickload '(:clops-gui) :silent )
+
+;;; package ================================================================
+(defpackage #:minimal
+  (:use #:cl)
+  (:import-from :serapeum
+                :~>)
+  (:import-from :defclass-std
+                :defclass/std))
+
+(in-package #:minimal)
+
+(defclass/std minimal-window (gui-window:lisp-window) (()))
+
+;;; drawing ====================================================================
+(defmethod draw-window ((window minimal-window))
+  ;; paint background
+  (let ((cv 0.13)) (cairo:set-source-rgb  cv cv cv))
+  (cairo:paint)
+
+  (cairo:select-font-face "Ubuntu Mono" :italic :bold)
+  (cairo:select-font-size 15)
+  (cairo:move-to 10 100)
+  (let ((cmotion    (gui-app:current-motion-window-p gui-app:*lisp-app* window)))
+    (if cmotion
+        (gui-window:set-rgba "green")
+        (gui-window:set-rgba "red"))
+    (cairo:show-text (format nil "motion ~A" cmotion)))
+
+  ;; pink square follows the mouse
+  (let ((app gui-app:*lisp-app*))
+    (when (and (eq (gui-app:current-motion app)
+                   window)
+               (gui-app:mouse-coordinates app))
+      (gui-window:set-rgba "pink")
+      (cairo:rectangle
+       (car (gui-app:mouse-coordinates app))
+       (cdr (gui-app:mouse-coordinates app))
+       25
+       25)
+      (cairo:fill-path))))
+
+;;; events =====================================================================
+(defmethod process-event ((lisp-window minimal-window) event &rest args)
+  (case event
+    (:timeout
+          ;; do nothing yet
+     )
+    ((:motion :motion-enter)
+          ;; we use simple case with one window so we ignore the window argument
+     (destructuring-bind ((x y)) args
+       (gui-app:mouse-motion-enter lisp-window x y)))
+    (:motion-leave
+          (gui-app:mouse-motion-leave))
+    (:focus-enter)
+    (:focus-leave)
+    (:pressed
+          (destructuring-bind ((button x y)) args
+            (declare (ignore button))
+            ))
+    (:released
+          (gui-app:mouse-button-released))
+    (:scroll)
+    (:resize
+          (destructuring-bind ((w h)) args
+            (gui-window:window-resize w h lisp-window)))
+    (:key-pressed
+          (destructuring-bind ((entered key-name key-code mods)) args
+            (format t "~&>>> key pressed ~S~%" (list entered key-name key-code mods))
+            ))
+    (otherwise
+          (warn "not handled event ~S ~S" event args)))
+
+  ;; redrawing ------------------------------
+  (gui-window:redraw-canvas lisp-window (format  nil "~A" event)))
+
+;;; main =======================================================================
+(defun main ()
+  (setf
+   gui-drawing:*client-fn-draw-objects*  'minimal::draw-window
+   gui-window-gtk:*client-fn-menu-bar*      nil
+   gui-events:*client-fn-process-event* 'minimal::process-event
+   gui-window-gtk:*initial-window-width*    600
+   gui-window-gtk:*initial-window-height*   400
+   gui-window-gtk:*initial-title*           "Minimal")
+
+  (gui-window-gtk:window (make-instance 'minimal-window)))
+
+;; (main)
