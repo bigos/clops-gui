8575eb2d65df46379aeca8ed3d3c4b3d5742a15b
Jacek Podkanski <ruby.object@googlemail.com>
2025-06-09 14:33:28 +0100

    added tests

---
+++ tests/package.lisp
@@ -0,0 +1,11 @@
+;;;
+(defpackage #:clops-gui-tests
+  (:use #:cl
+        #:fiveam
+        #:gui-app)
+  (:export #:run!
+           #:run-all-tests
+           #:clos-sweeper-suite)
+  (:import-from #:gui-app
+
+                ))
