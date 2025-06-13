b37e3fd8ced3a5c6de4ade8c265a2ddd2d3feb7b
Jacek Podkanski <ruby.object@googlemail.com>
2025-06-13 11:40:06 +0100

    noinform and exit

--- tests/terminal-test-loader.lisp
+++ tests/terminal-test-loader.lisp
@@ -1,7 +1,8 @@
-;;;
-(format t "~&running terminal test loader~%")
+;; sbcl --noinform --load ./terminal-test-loader.lisp
+
+(format t "~&running terminal test loader~%~%")
 
 (ql:quickload :clops-gui/tests)
 (asdf:test-system :clops-gui/tests)
 
-(format t "~&Press Ctrl-d to exit ~%")
+(sb-ext:quit)
