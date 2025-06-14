53a013223a4eb1a54c956e48d435e003a8ed6143
Jacek Podkanski <ruby.object@googlemail.com>
2025-06-14 14:51:42 +0100

    window works

--- examples/boxes-new.lisp
+++ examples/boxes-new.lisp
@@ -16,3 +16,96 @@
    :defclass/std))
 
 (in-package #:boxes-new)
+
+;;; minimal window -------------------------------------------------------------
+
+
+(defclass/std boxes-new-window (gui-window:lisp-window) (()))
+
+;;; drawing ====================================================================
+(defmethod draw-window ((window boxes-new-window))
+  ;; paint background
+  (let ((cv 0.95)) (cairo:set-source-rgb  cv cv cv))
+  (cairo:paint)
+
+  (cairo:select-font-face "Ubuntu Mono" :italic :bold)
+  (cairo:set-font-size 10)
+  (cairo:move-to 10 10)
+  (gui-window:set-rgba "black")
+  (cairo:show-text (format nil "try moving the mouse over the window and outside of it"))
+
+
+  (cairo:select-font-face "Ubuntu Mono" :italic :bold)
+  (cairo:set-font-size 15)
+  (cairo:move-to 10 100)
+  (let ((cmotion    (gui-app:current-motion-window-p gui-app:*lisp-app* window)))
+    (if cmotion
+        (gui-window:set-rgba "green")
+        (gui-window:set-rgba "red"))
+    (cairo:show-text (format nil "motion ~A" cmotion)))
+
+  ;; pink square follows the mouse
+  (let ((app gui-app:*lisp-app*))
+    (when (and (eq (gui-app:current-motion app)
+                   window)
+               (gui-app:mouse-coordinates app))
+      (gui-window:set-rgba "pink")
+      (cairo:rectangle
+       (car (gui-app:mouse-coordinates app))
+       (cdr (gui-app:mouse-coordinates app))
+       25
+       25)
+      (cairo:fill-path))))
+
+;;; events =====================================================================
+(defmethod process-event ((lisp-window boxes-new-window) event &rest args)
+  (unless (eq event :timeout)
+    (warn "event ~S ~S" event args))
+
+  (case event
+    (:timeout
+     ;; do nothing yet
+     )
+    ((:motion :motion-enter)
+     ;; we use simple case with one window so we ignore the window argument
+     (destructuring-bind ((x y)) args
+       (gui-app:mouse-motion-enter lisp-window x y)))
+    (:motion-leave
+     (gui-app:mouse-motion-leave))
+    (:focus-enter)
+    (:focus-leave)
+    (:pressed
+     (destructuring-bind ((button x y)) args
+       (declare (ignore button))
+       ))
+    (:released
+     (gui-app:mouse-button-released))
+    (:scroll)
+    (:resize
+     (destructuring-bind ((w h)) args
+       (gui-window:window-resize w h lisp-window)))
+    (:key-pressed
+     (destructuring-bind ((entered key-name key-code mods)) args
+       (format t "~&>>> key pressed ~S~%" (list entered key-name key-code mods))
+       ))
+    (otherwise
+     (warn "not handled event ~S ~S" event args)))
+
+  ;; moving widgets -------------------------
+  ;; (warn "may implement moving widgets in response to actions)
+  ;; redrawing ------------------------------
+  (gui-window:redraw-canvas lisp-window (format  nil "~A" event)))
+
+;;; main =======================================================================
+(defun main ()
+  (setf
+   gui-drawing:*client-fn-draw-objects*  'boxes-new::draw-window
+   gui-window-gtk:*client-fn-menu-bar*      nil
+   gui-events:*client-fn-process-event* 'boxes-new::process-event
+   gui-window-gtk:*initial-window-width*    600
+   gui-window-gtk:*initial-window-height*   400
+   gui-window-gtk:*initial-title*           "Boxes-New")
+
+  (gui-window-gtk:window (make-instance 'boxes-new-window)))
+
+(main)
