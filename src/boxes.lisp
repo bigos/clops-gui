0c1037d1a3440b5d785c9f7523194e1fa5ba8138
Jacek Podkanski <ruby.object@googlemail.com>
2025-09-01 03:37:16 +0100

    refined character inspector

--- src/boxes.lisp
+++ src/boxes.lisp
@@ -458,7 +458,8 @@
             )))
 
 (defmethod print-object ((obj node-character) stream)
-  (print-object-inner obj stream))
+  (print-unreadable-object (obj stream :type t :identity t)
+    (format stream "~S" (bchar obj))))
 
 (defmethod print-object ((obj coordinates) stream)
   (print-object-inner obj stream))
